a379423fb1180cdd23bc1a346e87401a14865047
aminehjamal <126798305+aminehjamal@users.noreply.github.com>
2023-04-26 16:49:30 +0400

    changed titles

---
+++ data.js
@@ -0,0 +1,28 @@
+export const featuredPosts = [
+    {
+      title: 'Cozy sofa',
+      date: 'Dec 2023',
+      description:
+        'Comfy sofa, 2 seater, ...',
+      image: 'https://images.unsplash.com/photo-1484101403633-562f891dc89a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80',
+      imageLabel: 'Image Text',
+    },
+    {
+      title: 'Washing machine',
+      date: 'Dec 2023',
+      description:
+        'Washing machine, used for 1 year..',
+      image: 'https://images.unsplash.com/photo-1626806787426-5910811b6325?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageLabel: 'Image Text',
+    },
+    {
+      title: 'Stylish Lamp',
+      date: 'Dec 2023',
+      description:
+        'Lamp IKEA, model..',
+      image: 'https://images.unsplash.com/photo-1578678809569-1a8ead9cb802?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageLabel: 'Image Text',
+    },
+  ];
+
+  export default {}
